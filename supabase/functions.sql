2fa6cc214e0c650596d0796247d2f8c4647688a8
maximzayviy-pixel <maximzayviy@gmail.com>
2025-09-26 14:06:12 +0300

    Delete supabase directory

--- supabase/functions.sql
+++
@@ -1,9 +0,0 @@
--- списание после подтверждения
-create or replace function debit_user_balance(p_user_id uuid, p_amount numeric)
-returns void as $$
-begin
-  update balances
-  set available_rub = available_rub - p_amount,
-      hold_rub = greatest(hold_rub - p_amount, 0)
-  where user_id = p_user_id;
-end; $$ language plpgsql;
--- supabase/functions_bonus.sql
+++
@@ -1,6 +0,0 @@
--- начисление бонусов
-create or replace function grant_bonus_balance(p_user_id uuid, p_amount numeric)
-returns void as $$
-begin
-  update balances set bonus_rub = bonus_rub + p_amount where user_id = p_user_id;
-end; $$ language plpgsql;
